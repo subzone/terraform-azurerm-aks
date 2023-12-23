2f1b5aff1465baa6497e26e2af75ea0c7de05d91
subzone <subzone@live.com>
2023-12-23 20:51:21 +0100

    Added vnet submodule

---
+++ modules/vnet/main.tf
@@ -0,0 +1,27 @@
+data "azurerm_resource_group" "resource_group" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.vnet_name
+  location            = data.azurerm_resource_group.resource_group.location
+  resource_group_name = data.azurerm_resource_group.resource_group.name
+}
+
+resource "azurerm_virtual_network" "this" {
+  name                = var.vnet_name
+  location            = data.azurerm_resource_group.resource_group.location
+  resource_group_name = data.azurerm_resource_group.resource_group.name
+  address_space       = var.cidr_space
+  dns_servers         = var.dns_servers
+
+  subnet {
+    name           = var.subnet_name
+    address_prefix = var.subnet_address_prefix
+    security_group = azurerm_network_security_group.nsg.id
+  }
+
+
+  tags = var.tags
+}
---
+++ modules/vnet/variables.tf
@@ -0,0 +1,39 @@
+variable "tags" {
+    type = map(any)
+    description = "tags to be used" 
+}
+
+variable "cidr_space" {
+    type = list(any)
+    description = "cidr for vnet"
+}
+
+variable "subnet_address_prefix" {
+  type = string
+  description = "adress space for subnet"
+}
+
+variable "dns_servers" {
+  type = list(any)
+  description = "dns servers to be used in subnet"
+}
+
+variable "vnet_name" {
+  type = string
+  description = "new vnet name"
+}
+
+variable "subnet_name" {
+  type = string
+  description = "new subnet name" 
+}
+
+variable "resource_group_name" {
+  type = string
+  description = "rg name"
+}
+
+variable "location" {
+  type = string
+  description = "location to deploy resources"
+}
