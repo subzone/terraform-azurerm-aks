e23c550ab911c95ceeb16a5ecf12c224556c12e6
subzone <subzone@live.com>
2023-12-24 09:32:50 +0100

    fix typos

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -6,8 +6,8 @@ resource "azurerm_network_security_group" "nsg" {
 
 resource "azurerm_virtual_network" "this" {
   name                = var.vnet_name
-  location            = data.azurerm_resource_group.resource_group.location
-  resource_group_name = data.azurerm_resource_group.resource_group.name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
   address_space       = var.cidr_space
   dns_servers         = var.dns_servers
 
